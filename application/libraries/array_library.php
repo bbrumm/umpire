1441a5d756a2d70009560dffc6e1b9943732a8d2
Ben <completeitprofessional@gmail.com>
2015-09-21 07:31:03 +1000

    Updates to array sorting

---
+++ application/libraries/array_library.php
@@ -0,0 +1,18 @@
+<?php
+
+function in_array_r($needle, $haystack, $strict = false) {
+    foreach ($haystack as $item) {
+        if (($strict ? $item === $needle : $item == $needle) || (is_array($item) && in_array_r($needle, $item, $strict))) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+
+function sortByOrder($a, $b) {
+    return $a['1'] - $b['1'];
+}
+
+?>
--- application/models/report_model.php
+++ application/models/report_model.php
@@ -1,4 +1,7 @@
 <?php
+	require_once('UserReport.php');
+	require_once('/../libraries/array_library.php');
+
 class report_model extends CI_Model {
 	public function __construct() {
 		$this->load->database();
@@ -6,7 +9,7 @@ class report_model extends CI_Model {
 	
 	public function get_report() {
 		//Only gets the test report at the moment.
-		require_once('UserReport.php');
+
 		
 		$reportToDisplay = new UserReport();
 		$reportToDisplay->setReportType(5);
@@ -30,17 +33,34 @@ class report_model extends CI_Model {
 	//Create distinct arrays for grouping purposes
 	private function getDistinctListForGrouping($pArrayFieldName, $pResultArray) {
 		$fieldList = array();
-		//print_r($pResultArray);
-		for ($i=0, $numItems = count($pResultArray); $i < $numItems; $i++) {
-	
-			$fieldList[0][$i] = $pResultArray[$i]['short_league_name'];
-			$fieldList[1][$i] = $pResultArray[$i]['club_name'];
-		}
 		
-		print_r($fieldList);
+		echo "<pre>";
+		//print_r($pResultArray);
+		echo "</pre>";
+		
+		for ($i=0, $numItems = count($pResultArray); $i < $numItems; $i++) {
+			//if (!in_array_r($pResultArray, $fieldList)) {
+				$fieldList[$i][0] = $pResultArray[$i]['short_league_name'];
+				$fieldList[$i][1] = $pResultArray[$i]['club_name'];
+			//}
+		}
+		echo "<pre>";
+		//print_r($fieldList);
+		echo "</pre>";
 		$uniqueFieldList = array_unique($fieldList, SORT_REGULAR );
-		return $uniqueFieldList;
+		
+		//array_multisort($uniqueFieldList[1], SORT_ASC, SORT_STRING);
+		
+		usort($uniqueFieldList, 'sortByOrder');
+		
+		echo "<pre>";
+		print_r($uniqueFieldList);
+		echo "</pre>";
 
+		
+		return $uniqueFieldList;
+		
+		
 	}
 	
 }
