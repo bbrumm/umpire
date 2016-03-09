585a86f7bbe0d7119a379c73a4e4e52ced507dcc
Ben <completeitprofessional@gmail.com>
2016-03-10 05:49:15 +1100

    Fixed CSS rotation issue in IE
    Added ms-transform tags to CSS to fix IE9 column heading rotation issue.

--- application/helpers/dompdf_helper.php
+++ application/helpers/dompdf_helper.php
@@ -4,7 +4,7 @@ function pdf_create($html, $filename='', $stream=TRUE, $DPI)
     require_once("dompdf/dompdf_config.inc.php");
     $dompdf = new DOMPDF();
     //Testing layout to fit on one page
-    $dompdf->set_option( 'dpi' , $DPI );
+    $dompdf->set_option('dpi', $DPI);
     
     $dompdf->load_html($html);
     $dompdf->set_paper('a3', 'landscape');
