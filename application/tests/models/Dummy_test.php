67aa74c562a760d672728d39fed2479626246dc6
Ben Brumm <ben@databasestar.com>
2018-10-07 16:21:00 +1100

    Added dummy test

---
+++ application/tests/models/Dummy_test.php
@@ -0,0 +1,15 @@
+<?php
+
+class DummyTest extends TestCase {
+	public function setUp() {
+        $this->resetInstance();
+
+
+    }
+    public function test_firstDummy() {
+    	$this->assertEquals(1, 1);
+    }
+
+
+}
+?>
