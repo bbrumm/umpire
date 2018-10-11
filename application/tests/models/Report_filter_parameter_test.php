70bea9ae98dfe4c36920b3df228355f00dae9d68
Ben <info@databasestar.com>
2018-10-11 12:27:02 +1100

    Create Report_filter_parameter_test.php

---
+++ application/tests/models/Report_filter_parameter_test.php
@@ -0,0 +1,28 @@
+<?php
+class Report_filter_parameter_test extends TestCase {
+public function setUp() {
+$this->resetInstance();
+$this->CI->load->model('Report_filter_parameter');
+$this->obj = $this->CI->Report_filter_parameter;
+}
+
+public function test_createFilterParameter_ValuesOKSQL() {
+$expected = "'First','Second','Third'";
+$inputFilterParam = array('First', 'Second', 'Third');
+$inputPDFMode = true;
+$inputRegion = false;
+$this->obj->createFilterParameter$inputFilterParam, $inputPDFMode, $inputRegion );
+$actual = $this->obj->getFilterSQLValues();
+$this->assertEquals($expected, $actual);
+}
+
+public function test_createFilterParameter_ValuesOKDisplay() {
+$expected = "First,Second,Third";
+$inputFilterParam = array('First', 'Second', 'Third');
+$inputPDFMode = true;
+$inputRegion = false;
+$this->obj->createFilterParameter$inputFilterParam, $inputPDFMode, $inputRegion );
+$actual = $this->obj->getFilterDisplayValues();
+$this->assertEquals($expected, $actual);
+}
+}
