4db9f9e6adf3ef9e1b46e5c912810e53b226c92d
Ben <info@databasestar.com>
2018-10-16 11:19:29 +1100

    Update Report7.php

--- application/models/separate_reports/Report7.php
+++ application/models/separate_reports/Report7.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'IReport.php';
 
-class Report7 extends CI_Model implements IReport {
+class Report7 extends Parent_report implements IReport {
     
     public function getReportDataQuery(Report_instance $pReportInstance) {
         $queryString = "SELECT ".
