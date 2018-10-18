6cf7ec5ad6d60067f58b9e4fe2a4885f8a013450
Ben <info@databasestar.com>
2018-10-19 09:07:09 +1100

    Update User_maintenance_model_test.php

--- application/tests/models/User_maintenance_model_test.php
+++ application/tests/models/User_maintenance_model_test.php
@@ -39,5 +39,16 @@ class User_maintenance_model_test extends TestCase
         $actual = $this->obj->validatePassword($firstPassword, $secondPassword);
         $this->assertEquals($expected, $actual);
     }
+    
+    public function test_CheckUserExistsForReset() {
+        $arrayStore = new Array_store;
+        $this->obj = new User_maintenance_model();
+        $user = createUserFromNameAndPW("test", "john", "smith", "mypass");
+        $expected = true;
+        $actual = $this->obj->checkUserExistsForReset($arrayStore, $user);
+        $this->assertEquals($expected, $actual);
+
+}
+
 
 }
