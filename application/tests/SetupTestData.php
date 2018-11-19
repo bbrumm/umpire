b3a14392990973d855cfe95c0ece1e34d3adae71
Ben Brumm <ben@databasestar.com>
2018-11-20 06:30:58 +1100

    Update yaml

---
+++ application/tests/SetupTestData.php
@@ -0,0 +1,12 @@
+<?php
+
+$queryString = "CREATE TABLE 'password_reset_request' (
+  'request_datetime' datetime,
+  'activation_id' varchar(200),
+  'ip_address' varchar(50),
+  'user_name' varchar(255),
+  'email_address' varchar(255)
+);";
+
+echo $queryString . " | mysql -u root";
+
