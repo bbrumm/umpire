905dd3012ca4145863608d0097f725fa716fa9fc
Ben Brumm <ben@databasestar.com>
2018-11-20 07:17:22 +1100

    Update data_setup.sql

--- application/tests/data_setup.sql
+++ application/tests/data_setup.sql
@@ -7,3 +7,20 @@ CREATE TABLE password_reset_request (
   user_name varchar(255),
   email_address varchar(255)
 );
+
+CREATE TABLE umpire_users (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  user_name varchar(255) NOT NULL,
+  user_email varchar(255) NOT NULL,
+  user_password varchar(255) NOT NULL,
+  first_name varchar(100) DEFAULT NULL,
+  last_name varchar(100) DEFAULT NULL,
+  activation_id varchar(20) DEFAULT NULL,
+  role_id int(11) DEFAULT NULL,
+  active int(1) DEFAULT NULL,
+  PRIMARY KEY (id)
+);
+
+
+INSERT INTO umpire_users (id, user_name, user_email, user_password, first_name, last_name, role_id, active)
+VALUES (24, 'bbrummtest', 'brummthecar@gmail.com', MD5('test'), 'Ben', 'Brumm', 3, 1);
