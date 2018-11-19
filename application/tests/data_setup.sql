29cccd4026f9a5806332807b68d319163b62f160
Ben Brumm <ben@databasestar.com>
2018-11-20 06:59:20 +1100

    Fixed syntax

--- application/tests/data_setup.sql
+++ application/tests/data_setup.sql
@@ -1,7 +1,7 @@
-CREATE TABLE 'password_reset_request' (
-  'request_datetime' datetime,
-  'activation_id' varchar(200),
-  'ip_address' varchar(50),
-  'user_name' varchar(255),
-  'email_address' varchar(255)
+CREATE TABLE password_reset_request (
+  request_datetime datetime,
+  activation_id varchar(200),
+  ip_address varchar(50),
+  user_name varchar(255),
+  email_address varchar(255)
 );
