cdbd3c65f965ef6c31344cd30e70426db48da832
Ben Brumm <ben@databasestar.com>
2018-12-10 06:15:03 +1100

    Update data_setup.sql

--- application/tests/data_setup.sql
+++ application/tests/data_setup.sql
@@ -1548,3 +1548,15 @@ SPLIT
 
 END$$
 DELIMITER ;
+
+
+DELIMITER $$
+CREATE PROCEDURE `LogTableOperation`(IN `pImportedFileID` INT, IN `pProcessedTableID` INT, IN `pOperationID` INT, IN `pRowCount` INT)
+BEGIN
+
+INSERT INTO table_operations (imported_file_id, processed_table_id, operation_id, operation_datetime, rowcount)
+VALUES (pImportedFileID, pProcessedTableID, pOperationID, NOW(), pRowCount);
+
+
+END$$
+DELIMITER ;
