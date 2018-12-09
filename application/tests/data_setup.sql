8e80a4c0da052ac375d0c7a28f95e3aeb6446a87
Ben <info@databasestar.com>
2018-12-10 08:53:08 +1100

    Added proc FindMissingData

--- application/tests/data_setup.sql
+++ application/tests/data_setup.sql
@@ -1738,3 +1738,33 @@ VALUES (pImportedFileID, pProcessedTableID, pOperationID, NOW(), pRowCount);
 
 END$$
 DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE `FindMissingData`()
+BEGIN
+
+DECLARE vSeasonYear INT(4);
+
+TRUNCATE TABLE incomplete_records;
+/*
+Find new or incomplete competition records.
+These records were inserted as part of the ETL job, but the league was not assigned, as it needs a user to confirm the values.
+*/
+
+INSERT INTO incomplete_records(record_type, source_id, source_value)
+SELECT DISTINCT 'competition', id, competition_name
+FROM competition_lookup
+WHERE league_id IS NULL;
+
+/*
+Find new or incomplete teams
+*/
+
+INSERT INTO incomplete_records(record_type, source_id, source_value)
+SELECT DISTINCT 'team', id, team_name
+FROM team
+WHERE club_id IS NULL;
+
+END$$
+DELIMITER ;
+		       
