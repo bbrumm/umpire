ee5501d2dd7ace5e8f3a8f806bae5b74873dbd5f
Ben Brumm <ben@databasestar.com>
2019-02-01 17:58:37 +1100

    Added more error tests

--- application/tests/controllers/ErrorView_test.php
+++ application/tests/controllers/ErrorView_test.php
@@ -7,7 +7,7 @@ class ErrorView_test extends TestCase
 
     }
 
-    public function testFileLoadCLI() {
+    public function testErrorGeneralCLI() {
         $exception = new CI_Exceptions();
         $heading = "Test heading 123";
         $message = "This message";
@@ -17,13 +17,71 @@ class ErrorView_test extends TestCase
         $this->assertContains($expectedOutput, $output);
     }
 
-    public function testFileLoad() {
-        $exception = new CI_Exceptions();
-        $heading = "Test heading 123";
-        $message = "This message";
+    public function testErrorGeneralHTML() {
         $output = $this->CI->load->view('errors/html/error_general', '', true);
 
         $expectedOutput = "<title>Error</title>";
+        $this->assertContains($expectedOutput, $output);
+    }
+
+    public function testError404CLI() {
+        $data['heading'] = "Test heading 123";
+        $data['message'] = "This message";
+        $output = $this->CI->load->view('errors/cli/error_404', $data, true);
+
+        $expectedOutput = "ERROR: " . $data['heading'];
+        $this->assertContains($expectedOutput, $output);
+    }
+
+    public function testError404HTML() {
+        $output = $this->CI->load->view('errors/html/error_404', '', true);
+
+        $expectedOutput = "<title>404 Page Not Found</title>";
+        $this->assertContains($expectedOutput, $output);
+    }
+
+    public function testErrorDBCLI() {
+        $data['heading'] = "Test heading 123";
+        $data['message'] = "This message";
+        $output = $this->CI->load->view('errors/cli/error_db', $data, true);
+
+        $expectedOutput = "Database error: " . $data['heading'];
+        $this->assertContains($expectedOutput, $output);
+    }
+
+    public function testErrorDBHTML() {
+        $output = $this->CI->load->view('errors/html/error_db', '', true);
+
+        $expectedOutput = "<title>Database Error</title>";
+        $this->assertContains($expectedOutput, $output);
+    }
+
+    public function testErrorExceptionCLI() {
+        $data['heading'] = "Test heading 123";
+        $data['message'] = "This message";
+        $data['exception'] = new Exception();
+        $output = $this->CI->load->view('errors/cli/error_exception', $data, true);
+
+        $expectedOutput = "An uncaught Exception was encountered";
+        $this->assertContains($expectedOutput, $output);
+    }
+
+    public function testErrorExceptionHTML() {
+        $data['exception'] = new Exception();
+        $output = $this->CI->load->view('errors/html/error_exception', $data, true);
+
+        $expectedOutput = "An uncaught Exception was encountered";
+        $this->assertContains($expectedOutput, $output);
+    }
+
+    public function testErrorPHPHTML() {
+        $data['severity'] = 1;
+        $data['message'] = "testmesssage";
+        $data['filepath'] = "abc";
+        $data['line'] = 2;
+        $output = $this->CI->load->view('errors/html/error_php', $data, true);
+
+        $expectedOutput = "<h4>A PHP Error was encountered</h4>";
         $this->assertContains($expectedOutput, $output);
     }
 
