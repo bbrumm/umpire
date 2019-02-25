3ef913e28ac6a565b936d719876bb0eddf019569
Ben <info@databasestar.com>
2019-02-25 14:43:24 +1100

    Update Report2.php

--- application/models/separate_reports/Report2.php
+++ application/models/separate_reports/Report2.php
@@ -141,6 +141,7 @@ class Report2 extends Parent_report implements IReport {
 
     public function pivotQueryArray($pResultArray, array $pFieldForRowLabel, array $pFieldsForColumnLabel) {
         $pivotedArray = array();
+	$previousRowLabel = array();
         $counterForRow = 0;
         $previousRowLabel[0] = "";
         foreach ($pResultArray as $resultRow) {
