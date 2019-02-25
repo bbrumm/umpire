cdff68f8c08dacfd3e07dc41b76272c5d00e0bfb
Ben <info@databasestar.com>
2019-02-25 16:14:12 +1100

    Update ImportFileSelector.php

--- application/controllers/ImportFileSelector.php
+++ application/controllers/ImportFileSelector.php
@@ -10,7 +10,8 @@ class ImportFileSelector extends CI_Controller {
 	}
 	
 	public function index() {
-	    $data['test'] = "Test Report";
+		$data = array();
+	        $data['test'] = "Test Report";
 		$this->load->view('templates/header', $data);
 		$this->load->view('upload_form', array('error' => ' ' , $data));
 		$this->load->view('templates/footer');
