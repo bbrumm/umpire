b2db79f1015e16da1e90e16464b844e53709ea05
Ben <info@databasestar.com>
2019-04-23 13:53:03 +1000

    Create ReportCell_test.php

---
+++ application/tests/models/ReportCell_test.php
@@ -0,0 +1,16 @@
+<?php
+class ReportCell_Test extends TestCase {
+
+  public function setUp() {
+    $this->CI->load->model('Report_cell');
+    $this->obj = $this->CI->Report_cell;
+  }
+
+  public function test_SetCellValue_String() {
+    $inputValue = "This is a test value";
+    $this->obj->setCellValue($inputValue);
+
+    $actualValue = $this->obj->getCellValue();
+    $this->assertEquals($inputValue, $actualValue);
+  }
+}
