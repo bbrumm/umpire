80e7ae7b469b765e6b11045ed61612e7eccee0e1
Ben <info@databasestar.com>
2019-06-13 14:25:51 +1000

    Update Report_table_refresher_test.php

--- application/tests/models/Report_table_refresher_test.php
+++ application/tests/models/Report_table_refresher_test.php
@@ -12,5 +12,39 @@ class Report_table_refresher_test extends TestCase {
         $this->obj->disableKeys();
     }
     
+     public function test_DisableKeysForSpecificTable_Exception() {
+        $this->expectException(Exception::class);
+        $this->obj->disableKeysForSpecificTable(null);
+    }
+    
+    public function test_EnableKeys_Exception() {
+        $this->expectException(Exception::class);
+        $this->obj->setTableName(null);
+        $this->obj->enableKeys();
+    }
+    
+    public function test_EnableKeysForSpecificTable_Exception() {
+        $this->expectException(Exception::class);
+        $this->obj->enableKeysForSpecificTable(null);
+    }
+    
+    public function test_TableInsertOperation_Exception() {
+        $this->expectException(Exception::class);
+        $this->obj->setTableName(null);
+        $this->obj->logTableInsertOperation();
+    }
+    
+    public function test_TableUpdateOperation_Exception() {
+        $this->expectException(Exception::class);
+        $this->obj->setTableName(null);
+        $this->obj->logTableUpdateOperation();
+    }
+    
+    public function test_TableDeleteOperation_Exception() {
+        $this->expectException(Exception::class);
+        $this->obj->setTableName(null);
+        $this->obj->logTableDeleteOperation();
+    }
+    
     
 }
