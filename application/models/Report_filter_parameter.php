d9dd71491d65b4d50730d58a663ce58a82b19c5b
Ben <info@databasestar.com>
2019-06-14 12:03:31 +1000

    Update Report_filter_parameter.php

--- application/models/Report_filter_parameter.php
+++ application/models/Report_filter_parameter.php
@@ -9,38 +9,45 @@ class Report_filter_parameter extends CI_Model {
     }
     
     public function createFilterParameter($pFilterParameter, $pdfMode, $pFilterIsRegion = false) {
-        //$this->debug_library->debugOutput("pFilterParameters in createFilterParameter: ", $pFilterParameter);
-        //$this->debug_library->debugOutput("pFilterParameters isRegion: ", $pFilterIsRegion);
         if ($pdfMode) {
-            $this->filterSQLValues = $this->strReplaceWithApostrophe($pFilterParameter);
-            $this->filterDisplayValues = $this->strReplaceWithoutApostrophe($pFilterParameter);
+            $this->createParameterForPdfMode($pFilterParameter);
         } elseif ($pFilterIsRegion) {
-            
-            //This Region value can sometimes be an array, or a single value.
-            //TODO: Commented out the first line and uncommented the first line, 
-            //because selecting a single Region causes the query to return nothing for report 1.
-            //Come back here if there are still errors.
-            if(is_array($pFilterParameter)) {
-                $pFilterParameterRegion = $pFilterParameter[0];
-            } else {
-                $pFilterParameterRegion = $pFilterParameter;
-            }
-            
-            //$this->filterSQLValues = "'" . $pFilterParameter[0] . "'";            
-            $this->filterSQLValues = $this->strReplaceWithApostrophe($pFilterParameterRegion);
-            $this->filterDisplayValues = $pFilterParameterRegion;
+            $this->createParameterForRegion($pFilterParameter);
         } else {
-            if(is_array($pFilterParameter)) {
-            //$this->debug_library->debugOutput("pFilterParameters ready to implode: ", $pFilterParameter);
-                if (count($pFilterParameter) > 0) {
-                    $this->filterSQLValues = $this->implodeWithApostrophe($pFilterParameter);
-                    $this->filterDisplayValues = $this->implodeWithoutApostrophe($pFilterParameter);
-                } else {
-                    throw new InvalidArgumentException("The provided filterParameter array is empty.");
-                }
+            $this->createParameterForRegularArray($pFilterParameter);
+        }
+    }
+    
+    private function createParameterForPdfMode($pFilterParameter) {
+        $this->filterSQLValues = $this->strReplaceWithApostrophe($pFilterParameter);
+        $this->filterDisplayValues = $this->strReplaceWithoutApostrophe($pFilterParameter);
+    }
+    
+    //This Region value can sometimes be an array, or a single value.
+    /*Commented out the first line and uncommented the first line, 
+    because selecting a single Region causes the query to return nothing for report 1.
+    Come back here if there are still errors.
+    */
+    private function createParameterForRegion($pFilterParameter) {
+        if(is_array($pFilterParameter)) {
+            $pFilterParameterRegion = $pFilterParameter[0];
+        } else {
+            $pFilterParameterRegion = $pFilterParameter;
+        }
+        $this->filterSQLValues = $this->strReplaceWithApostrophe($pFilterParameterRegion);
+        $this->filterDisplayValues = $pFilterParameterRegion;
+    }
+    
+    private function createParameterForRegularArray($pFilterParameter) {
+        if(is_array($pFilterParameter)) {
+            if (count($pFilterParameter) > 0) {
+                $this->filterSQLValues = $this->implodeWithApostrophe($pFilterParameter);
+                $this->filterDisplayValues = $this->implodeWithoutApostrophe($pFilterParameter);
             } else {
-                throw new InvalidArgumentException("The provided filterParameter value is expected to be an array, but a string was provided: ". $pFilterParameter);
+                throw new InvalidArgumentException("The provided filterParameter array is empty.");
             }
+        } else {
+            throw new InvalidArgumentException("The provided filterParameter value is expected to be an array, but a string was provided: ". $pFilterParameter);
         }
     }
     
