e6e89657e69bfe06b79a624452ad45285e5e7007
Ben <info@databasestar.com>
2019-06-19 15:51:45 +1000

    Update User_role_permission.php

--- application/models/useradmin/User_role_permission.php
+++ application/models/useradmin/User_role_permission.php
@@ -1,10 +1,7 @@
 <?php
-
-class User_role_permission extends CI_Model
-{
+class User_role_permission extends CI_Model {
     
-    function __construct()
-    {
+    function __construct() {
         parent::__construct();
     }
 
@@ -15,59 +12,58 @@ class User_role_permission extends CI_Model
 
     public static function createFromRow($pRow) {
         $instance = new User_role_permission();
-        if($pRow['id'] != null &&
-            $pRow['permission_id'] != null &&
-            $pRow['permission_name'] != null &&
-            $pRow['selection_name'] != null) {
-
-            $instance->setId($pRow['id']);
-            $instance->setPermissionId($pRow['permission_id']);
-            $instance->setPermissionName($pRow['permission_name']);
-            $instance->setSelectionName($pRow['selection_name']);
-
-            return $instance;
-        } else {
-            throw new InvalidArgumentException("Data used for creating user_role_permission is null.");
-        }
-
+        $instance->setId($pRow['id']);
+        $instance->setPermissionId($pRow['permission_id']);
+        $instance->setPermissionName($pRow['permission_name']);
+        $instance->setSelectionName($pRow['selection_name']);
+        return $instance;
     }
 
     public function getId() {
         return $this->id;
     }
     
-    public function getPermissionId()
-    {
+    public function getPermissionId() {
         return $this->permissionId;
     }
 
-    public function getPermissionName()
-    {
+    public function getPermissionName() {
         return $this->permissionName;
     }
 
-    public function getSelectionName()
-    {
+    public function getSelectionName() {
         return $this->selectionName;
     }
 
-    public function setPermissionId($pValue)
-    {
-        $this->permissionId = $pValue;
+    public function setPermissionId($pValue) {
+        if ($pValue != null) {
+            $this->permissionId = $pValue;
+        } else {
+            throw new InvalidArgumentException("PermissionID used for creating user_role_permission is null.");
+        }
     }
 
-    public function setPermissionName($pValue)
-    {
-        $this->permissionName = $pValue;
+    public function setPermissionName($pValue) {
+        if ($pValue != null) {
+            $this->permissionName = $pValue;
+        } else {
+            throw new InvalidArgumentException("PermissionName used for creating user_role_permission is null.");
+        }
     }
 
-    public function setSelectionName($pValue)
-    {
-        $this->selectionName = $pValue;
+    public function setSelectionName($pValue) {
+        if ($pValue != null) {
+            $this->selectionName = $pValue;
+        } else {
+            throw new InvalidArgumentException("SelectionName used for creating user_role_permission is null.");
+        }
     }
     
     public function setId($pValue) {
-        $this->id = $pValue;
-    }
-        
+        if ($pValue != null) {
+            $this->id = $pValue;
+        } else {
+            throw new InvalidArgumentException("ID used for creating user_role_permission is null.");
+        }
+    }  
 }
